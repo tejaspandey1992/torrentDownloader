385042784a096a256c6ac94b1921e45a133ddaa8
Tejas Pandey <tejaspandey2010@gmail.com>
2016-05-03 12:58:49 +0530

    updating version install script

--- install/install.sh
+++ install/install.sh
@@ -13,7 +13,7 @@ sudo apt-get -y install transmission-daemon
 #sudo apt-get -y install vim
 sudo pip install --upgrade google-api-python-client
 sudo pip install --upgrade python-gflags
-sudo pip install torrentDownloader-1.0.tar.gz
+sudo pip install torrentDownloader-2.0.tar.gz
 sudo chmod 777 $install/*.txt $install/*.yaml  $install/*.py $install/*.json $install/*.storage
 sudo service  transmission-daemon stop
 sudo chmod 777 /etc/transmission-daemon/settings.json
